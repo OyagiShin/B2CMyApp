9b493c5e4a25952ff09683e1984d950f769c7b97
晋 大八木 <ooyagi.shin@gmail.com>
2019-12-29 17:00:13 +0900

    test

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace WebApplication1.Controllers {
     public class HomeController : Controller {
         public IActionResult Index( int? id) {
 
-            // test
+            // test 123344
             return View();
         }
     }
